40039c74ba659eb1cc64d2123d14ae4bc50cf2d9
Roman <roman.kysil@redturtle.it>
2024-08-21 10:43:37 +0200

    Back to development: 1.1.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.feedback",
-    version="1.1.4",
+    version="1.1.5.dev0",
     description="Feedback mechanism integration for io-comune",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
